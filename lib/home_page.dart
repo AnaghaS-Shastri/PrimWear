3c866bca932fcdb312c995e10a109c121944cc3e
AnaghaS-Shastri <149950166+AnaghaS-Shastri@users.noreply.github.com>
2024-09-28 13:08:19 +0530

    platform to code more

---
+++ lib/home_page.dart
@@ -0,0 +1,49 @@
+import 'package:flutter/material.dart';
+import 'first_page.dart';
+import 'second_page.dart';
+import 'third_page.dart';
+
+class HomePage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Home Page'),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            ElevatedButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => FirstPage()),
+                );
+              },
+              child: Text('Go to First Page'),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => SecondPage()),
+                );
+              },
+              child: Text('Go to Second Page'),
+            ),
+            ElevatedButton(
+              onPressed: () {
+                Navigator.push(
+                  context,
+                  MaterialPageRoute(builder: (context) => ThirdPage()),
+                );
+              },
+              child: Text('Go to Third Page'),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
